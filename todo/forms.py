a50251be14e5d92649abceda50d756e437e6eb2d
JimYang <chungki082485@gmail.com>
2021-10-20 21:58:38 +0800

    myproject
    form validation

--- todo/forms.py
+++ todo/forms.py
@@ -3,46 +3,48 @@ from .models import Todo, Project
 from django.core.validators import RegexValidator
 from django.forms import ModelForm, Textarea
 from django.utils import timezone
+from datetime import datetime
 
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ('taskName', 'taskDescription',
+        fields = ('taskName', 'taskDescription', 'taskCreate',
                   'taskDueDate', 'taskPriority', 'taskComplete', 'project', 'employee')
+        widgets = {
+            'taskDescription': Textarea(attrs={'cols': 40, 'rows': 10, 'placeholder': 'Write some descriptions about the task'}),
+            'taskName': forms.TextInput(attrs={'size': '40', 'placeholder': 'eg. Create mysql database'}),
+            'taskDueDate': forms.DateInput(attrs={'type': 'date'}),
+        }
 
-    # def clean_taskName(self):
+    def clean(self):
+        cleaned_data = super().clean()
+        end_date = cleaned_data.get("taskDueDate")
+        if end_date <= timezone.now().date():
+            raise forms.ValidationError(
+                "TaskDueDate cannot be in the past")
 
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ('projectName', 'projectCreate', 'projectDescription',
-                  'creationDate', 'endDate', 'employee')
+                  'startDate', 'endDate', 'employee')
         widgets = {
             'projectDescription': Textarea(attrs={'cols': 40, 'rows': 10, 'placeholder': 'Write some descriptions about the project'}),
             'projectName': forms.TextInput(attrs={'size': '40', 'placeholder': 'eg. todolist project'}),
-            'creationDate': forms.DateInput(attrs={'type': 'date'}),
+            'startDate': forms.DateInput(attrs={'type': 'date'}),
             'endDate': forms.DateInput(attrs={'type': 'date'}),
             'employee': forms.CheckboxSelectMultiple(),
         }
 
-    def clean_date1(self):
+    def clean(self):
         cleaned_data = super().clean()
-        start_date = cleaned_data.get("creationDate")
+        start_date = cleaned_data.get("startDate")
         end_date = cleaned_data.get("endDate")
-        if end_date < start_date:
+        if end_date <= start_date:
             raise forms.ValidationError(
-                "End date should be greater than start date.")
-
-    def clean_date2(self):
-        date = self.cleaned_data['creationDate']
-        if date < timezone.now().date():
-            raise forms.ValidationError("Date cannot be in the past")
-
-    # def clean_projectName(self):
-    #     projectname = self.cleaned_data["projectName"]
-    #     if len(projectname) > 20:
-    #         raise forms.ValidationError(
-    #             'The max length of firstName is 20 characters')
-    #     return projectname
+                "EndDate should be greater than startDate.")
+        # if start_date < timezone.now().date():
+        #     raise forms.ValidationError(
+        #         "Date cannot be in the past")
