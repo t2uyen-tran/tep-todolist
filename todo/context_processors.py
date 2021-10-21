b744587cbb6ee108cf6ac1bc9255c436bd89e7d4
Uyen Tran <tranthitouyen@gmail.com>
2021-10-21 12:16:25 +0800

    Fix AnonymousUser error

--- todo/context_processors.py
+++ todo/context_processors.py
@@ -3,17 +3,20 @@
 """
 
 import datetime
+
+from django.contrib.auth.decorators import login_required
+
 from todo.models import Todo
 
-
 def add_variable_to_context(request):
+    count = 0
+    if request.user.is_authenticated:
+        # filter the tasks that will be due in 14 days
+        deadline = datetime.date.today() + datetime.timedelta(days=14)
+        todos = Todo.objects.filter(employee=request.user).filter(taskDueDate__lte=deadline)
 
-    # filter the tasks that will be due in 14 days
-    deadline = datetime.date.today() + datetime.timedelta(days=14)
-    todos = Todo.objects.filter(employee=request.user).filter(taskDueDate__lte=deadline)
-
-    # count the number of upcoming tasks that due in 14 days
-    count = todos.count()
+        # count the number of upcoming tasks that due in 14 days
+        count = todos.count()
 
     return {
         'count': count
