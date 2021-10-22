2b0e8faf63c5c80e237e99c0fbffe5c5f8bb8383
JimYang <chungki082485@gmail.com>
2021-10-22 12:55:04 +0800

    myproject
    sort

--- todo/views/tasks.py
+++ todo/views/tasks.py
@@ -16,7 +16,7 @@ def todo_list(request):
     return render(request, "todo/todo_list.html", context)
 
 
-@login_required # Tracy
+@login_required  # Tracy
 def todo_create(request):
     form = TodoForm(request.POST or None)
     if form.is_valid():
@@ -29,7 +29,7 @@ def todo_create(request):
     return render(request, "todo/todo_create.html", context)
 
 
-@login_required # Tracy
+@login_required  # Tracy
 def todo_detail(request, id):
     todo = Todo.objects.get(id=id)
     context = {
@@ -38,7 +38,7 @@ def todo_detail(request, id):
     return render(request, "todo/todo_detail.html", context)
 
 
-def todo_update(request, id): # Tracy
+def todo_update(request, id):  # Tracy
     todo = Todo.objects.get(id=id)
     form = TodoForm(request.POST or None, instance=todo)
     if form.is_valid():
@@ -49,6 +49,7 @@ def todo_update(request, id): # Tracy
     }
     return render(request, "todo/todo_update.html", context)
 
+
 @login_required
 def todo_delete(request, id):
     todo = Todo.objects.get(id=id)
@@ -56,9 +57,11 @@ def todo_delete(request, id):
     project = Project.objects.get(id=key)
     if (request.user.id == todo.taskCreate_id) or (request.user.id == project.projectCreate_id):
         todo.delete()
-        messages.success(request, "Task '{}' has been deleted".format(todo.taskName))
+        messages.success(
+            request, "Task '{}' has been deleted".format(todo.taskName))
     else:
-        messages.warning(request, "You do not allow to delete the task '{}'.".format(todo.taskName))
+        messages.warning(
+            request, "You do not allow to delete the task '{}'.".format(todo.taskName))
     return redirect("/todo/mytask")
 
 
@@ -119,7 +122,8 @@ def todo_myprojecttask(request, id):
     }
     return render(request, "todo/todo_myprojectTask.html", context)
 
-@login_required # Tracy
+
+@login_required  # Tracy
 def todo_search(request):
     todos = Todo.objects.all()
     query_string = request.GET.get('search')
@@ -127,26 +131,29 @@ def todo_search(request):
     found_tasks = None
     key = None
     if query_string:
-        found_tasks = Todo.objects.filter( 
-                Q(taskName__icontains=query_string) | Q(taskDescription__icontains=query_string)
-            )
+        found_tasks = Todo.objects.filter(
+            Q(taskName__icontains=query_string) | Q(
+                taskDescription__icontains=query_string)
+        )
     else:
         error_msg = 'Please enter keywords'
-   
+
     if not request.user.is_superuser:
         for todo in todos:
-            if todo.employee_id!=request.user.id:
+            if todo.employee_id != request.user.id:
                 key = todo.id
                 found_tasks = found_tasks.exclude(id=key)
 
-    context = {"error_msg": error_msg, "query_string": query_string, "found_tasks": found_tasks}
+    context = {"error_msg": error_msg,
+               "query_string": query_string, "found_tasks": found_tasks}
     return render(request, "todo/todo_search.html", context)
+
 
 def todo_sortp(request):  # Tracy
     todos = Todo.objects.order_by('taskPriority')
     if not request.user.is_superuser:
         for todo in todos:
-            if todo.employee_id!=request.user.id:
+            if todo.employee_id != request.user.id:
                 key = todo.id
                 todos = todos.filter(~Q(id=key))
     context = {
@@ -154,14 +161,23 @@ def todo_sortp(request):  # Tracy
     }
     return render(request, "todo/todo_sortp.html", context)
 
+
 def todo_sortdate(request):  # Tracy
     todos = Todo.objects.order_by('taskDueDate')
     if not request.user.is_superuser:
         for todo in todos:
-            if todo.employee_id!=request.user.id:
+            if todo.employee_id != request.user.id:
                 key = todo.id
                 todos = todos.filter(~Q(id=key))
     context = {
         "todo_list": todos
     }
     return render(request, "todo/todo_sortdate.html", context)
+
+
+def todo_sortmyprojectdate(request):  # Tracy
+    projects = Project.objects.order_by('endDate')
+    context = {
+        "project_list": projects
+    }
+    return render(request, "todo/todo_sortmyprojectdate.html", context)
