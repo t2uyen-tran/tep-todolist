d9eacefb78e0a9cd51fb31d24bcb55ef30c42bc0
Uyen Tran <tranthitouyen@gmail.com>
2021-11-06 06:26:00 +0800

    Clean up the authen code

--- todo/admin.py
+++ todo/admin.py
@@ -1,6 +1,6 @@
 #####################################################################################################
-# TTUT:site administration use Django admin site to manage users and tasks                          #
-# Description: only for internal data management (i.e. just for use by admins, or staffers          #
+# Uyen:site administration use Django admin site to manage users and tasks                          #
+# Description: only for internal data management by superuser                                       #
 # Functions: to display and edit model data                                                         #
 # Constraints: only "superuser" and "Staff" account can access to the admin site                    #
 #####################################################################################################
